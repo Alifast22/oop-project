88645ab217971c883ac95a11fbafc166068d7fc9
Alifast22 <77858291+Alifast22@users.noreply.github.com>
2021-05-11 06:54:06 +0500

    Add files via upload

---
+++ classes.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+class airplane
+{
+	
+};
+
+class user
+{
+	string name;
+	int age;
+	int contact_num;
+	string gender;
+	
+};
+class plane:public airplane
+{
+	public:
+		
+	void bookflight()
+	{
+		
+	}
+	
+	void modifyflight()
+	{
+		
+	}
+	void cancelflight()
+	{
+		
+	}
+	
+	void annoucements()
+	{
+		
+	}
+};
+class finance_account
+{
+	string bankname;
+	int pin;
+	float money;
+	
+};
+
+class customer:public user,public plane,public finance_account
+{
+	int contact;
+	string hotel;
+	
+	public:
+	
+	void payment_method()
+	{
+		
+	}
+	void print_receipt()
+	{
+		
+	}
+};
+
+
+int main() {
+  
+ 
+}
